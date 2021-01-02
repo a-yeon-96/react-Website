68bae3358dcb3d9fdf3c2782d3131513fd3b1785
a-yeon-96 <68648964+a-yeon-96@users.noreply.github.com>
2021-01-02 16:08:37 +0900

    최종커밋확인

--- src/movie.js
+++ src/movie.js
@@ -1,34 +1,75 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
+import LinesEllipsis from 'react-lines-ellipsis'
 import './movie.css';
 
-class movie extends Component {
-
-    static propTypes = {
-        title: PropTypes.string.isRequired,  //isRequired movie는 title이라는 prop을 제공하는 것이 필수로 설정
-        poster: PropTypes.string.isRequired
-    }
-
-    render() {
+function movie ({title, poster, genres, synopsis}) {
         return (
-            <div>
-                <MoviePoster poster={this.props.poster}/>
-            <h1>{this.props.title}</h1>
+            <div className="Movie">
+                <div className="Movie_Column">
+                    <MoviePoster poster={poster} alt={title} />
+                </div>
+                <div className="Movie_Column">
+                    <h1>{title}</h1>
+                    <div className="Movie_Genres">
+                        {genres.map((genre, index) => <MovieGenre genre={genre} key={index} />)}
+                    </div>
+                    <p className="Movie_synopsis">
+                        <LinesEllipsis 
+                        text={synopsis}
+                        maxLine='3'
+                        ellipsis='...'
+                        trimRight
+                        basedOn='letters'
+                        />
+                    </p>
+                </div>
             </div>
         )
-    }
 }
 
-class MoviePoster extends Component {
-
-    static propTypes = {
-        poster: PropTypes.string.isRequired  //isRequired movie는 poster이라는 prop을 제공하는 것이 필수로 설정
-    }
-
-    render() {
-        return (
-            <img src={this.props.poster} alt="Movie Poster"/>
-        )
-    }
+function MoviePoster({poster, alt}){ // 멍청한 컴포먼트
+    return (
+        <img src={poster} alt={alt} title={alt} className="Movie_Poster"/>
+    )
 }
+
+function MovieGenre({genre}){ // 멍청한 컴포먼트
+    return (
+        <span className="Movie_Genre">{genre}</span>
+    )
+}
+
+
+movie.propTypes = {
+    title: PropTypes.string.isRequired,  //isRequired movie는 title이라는 prop을 제공하는 것이 필수로 설정
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.array.isRequired,
+    synopsis: PropTypes.string.isRequired
+}
+
+
+MoviePoster.propTypes = {
+    poster: PropTypes.string.isRequired,
+    alt: PropTypes.string.isRequired
+}
+
+
+MovieGenre.propTypes = {
+    genre: PropTypes.string.isRequired
+}
+
+// class MoviePoster extends Component { // 똑똑한 컴포먼트
+
+//     static propTypes = {
+//         poster: PropTypes.string.isRequired  //isRequired movie는 poster이라는 prop을 제공하는 것이 필수로 설정
+//     }
+
+//     render() {
+//         return (
+//             <img src={this.props.poster} alt="Movie Poster"/>
+//         )
+//     }
+//  }
+
 export default movie;
